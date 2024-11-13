d662cd60922802e6d0adcecf1bb9fc3f8dce6578
baroordp <169070181+baroordp@users.noreply.github.com>
2024-11-14 02:22:58 +0530

    Update vars.py

--- biisal/vars.py
+++ biisal/vars.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 bot_name = "Bɪɪsᴀʟ Fɪʟᴇ2Lɪɴᴋ Bᴏᴛ"
-bisal_channel = "https://telegram.me/bisal_files"
-bisal_grp = "https://t.me/+PA8OPL2Zglk3MDM1"
+bisal_channel = "https://t.me/erdgyas"
+bisal_grp = "https://t.me/+aNkfmSAbyFdkNjc9"
 
 class Var(object):
     MULTI_CLIENT = False
